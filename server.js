7d444c148c9b8ee32e90ae8f65cecda97e175710
chaseshobhit <114729391+chaseshobhit@users.noreply.github.com>
2024-11-09 16:56:07 +0530

    Fetching data from the database using Object_Id

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
@@ -39,9 +40,16 @@ app.post("/submit",async (req, res) => {
 
 
 app.get('/user/:id',async(req,res)=>{
+
+    const userId = req.params.id
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+        return res.status(400).json({message:"Invalid Id format"})
+    }
+
+
     try{
-        const userId = req.params.id
-        const user = await User.findByID(userId)
+        
+        const user = await User.findById(userId)
 
         if(!user){
             return res.status(404).json({message: 'User not found'})
@@ -52,12 +60,12 @@ app.get('/user/:id',async(req,res)=>{
             message: 'User data fetched successfully',
             data: user
         })
-    }catch(err){
-        console.error(`Error fetching data: ${err}`);
-        if(error.kind === 'ObjectId'){
-            return res.status(400).json({error: 'Invalid id format'})
-        }
-        res.status(500).json({err: 'Failed to fetch user data'})
+    }catch(error){
+        console.error(`Error fetching data: ${error}`);
+        // if(error.kind === 'ObjectId'){
+        //     // return res.status(400).json({error: 'Invalid id format'})
+        // }
+        res.status(500).json({message: 'Failed to fetch user data',error: error.message})
     }
 })
 
